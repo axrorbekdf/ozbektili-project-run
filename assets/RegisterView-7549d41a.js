e008deae3d2334de1e64b90f39b33ffd2a1fe138
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-29 13:21:41 +0500

    project run

---
+++ assets/RegisterView-7549d41a.js
@@ -0,0 +1,1 @@
+import{_ as o,R as s,r as t,o as r,h as n}from"./index-f5aab8aa.js";const c={components:{Register:s}};function a(p,_,i,f,m,R){const e=t("Register");return r(),n(e)}const g=o(c,[["render",a]]);export{g as default};
